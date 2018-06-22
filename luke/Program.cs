32b6e9ac9457d2b397944dba05aa82493ab9322b
Jarryd Goodman <>
2018-06-22 16:06:54 +0200

    Initial commit: two console applications talking over RabbitMQ

---
+++ luke/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace luke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who are you?");
+            string name = Console.ReadLine();
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new System.Uri("amqp://dvquwasp:uhWWbvjH8vteZQXRShimYxHeNNyW02OR@wombat.rmq.cloudamqp.com/dvquwasp");
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello-father",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                string message = $"Hello my name is, {name}.";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "hello-father",
+                    basicProperties: null,
+                    body: body
+                );
+            }
+
+            Console.WriteLine("Message sent to: [not-your-father]");
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ vader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace vader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new System.Uri("amqp://dvquwasp:uhWWbvjH8vteZQXRShimYxHeNNyW02OR@wombat.rmq.cloudamqp.com/dvquwasp");
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello-father",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Message received: \"{message}\"");
+                };
+                channel.BasicConsume(
+                    queue: "hello-father",
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine("Now receiving new children");
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
